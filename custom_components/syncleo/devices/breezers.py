6e4167d7a87e577b4081fffcbb1143b6b500b0c8
v.vadim <89260524+v-vadim@users.noreply.github.com>
2026-09-19 18:03:10 +0300

    Add climate support and ASP-100 specific backlight

--- custom_components/syncleo/devices/breezers.py
+++ custom_components/syncleo/devices/breezers.py
@@ -12,6 +12,7 @@ from pysyncleo.enums import UdpCommandType
 
 from ..const import (
     FEATURE_BACKLIGHT,
+    FEATURE_BREEZER_AUTO_OFF_BACKLIGHT,
     FEATURE_BREEZER_DAMPER,
     FEATURE_BREEZER_MELODY,
     FEATURE_BREEZER_TEMPERATURE,
@@ -333,7 +334,7 @@ PROFILES = [
                 }
             ),
         },
-        switches=[FEATURE_BACKLIGHT, FEATURE_VOLUME],
+        switches=[FEATURE_BREEZER_AUTO_OFF_BACKLIGHT, FEATURE_VOLUME],
         sensors={
             FEATURE_CURRENT_TEMPERATURE: SensorConfig(
                 device_class=SensorDeviceClass.TEMPERATURE,
